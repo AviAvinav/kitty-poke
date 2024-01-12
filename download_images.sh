8fa645a6cc32881c2ac5851a3ede762d3c41e7a7
Avi Avinav <aviavinav2004@gmail.com>
2024-01-12 23:12:27 +0530

    made the script work somehow on my system for now

--- download_images.sh
+++ download_images.sh
@@ -2,10 +2,13 @@ for i in {1..1024}
 do 
   curl -sL https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$i.png > pokemons/$i.png
   echo "Downloaded image number $1"
-done
+  magick ./pokemons/$i.png -resize 400% ./pokemons/$i.png
+  echo "Resized image number" $i
 
-echo ""
-echo ""
-echo "--------------------"
-echo "Done!"
-echo "--------------------"
+  echo ""
+  echo ""
+  echo "--------------------"
+  echo "Done!"
+  echo "--------------------"
+
+done
---
+++ poke.sh
@@ -0,0 +1,3 @@
+random=$((1 + $RANDOM % 1024))
+
+kitten icat --align left  ~/kitty-poke/pokemons/$random.png
